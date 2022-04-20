483523641b6e4fbaa61418006830a97f4b9f865b
choijeaho86@gmail.com <choijeaho86@gmail.com>
2022-04-20 18:25:18 +0900

    first commit

---
+++ src/aws/aws.controller.ts
@@ -0,0 +1,32 @@
+// /src/aws/aws.controller.ts
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AwsService } from './aws.service';
+
+@ApiTags('AWS S3 버킷으로 파일을 올리기')
+@Controller('aws')
+export class AwsController {
+  constructor(private readonly awsService: AwsService) {}
+
+  @Get('aws/upload/storage/file')
+  @ApiOperation({
+    summary: 'AWS S3버킷으로 저장된 파일을 전송합니다.',
+  })
+  async uploadStorage(
+    @Query('fileName') fileName: string,
+    @Query('saveName') saveName: string,
+  ) {
+    // 해당 파일을 전송 가능하도록 binary로 변환한다
+    const binary = this.awsService.convertBinary(fileName);
+
+    // 필요 내용을 file Object로 저장한다.
+    const file = {
+      buffer: binary,
+      savename: saveName,
+      mimetype: 'application/octet-stream',
+    };
+
+    // 업로드를 실행하고 해당 return값을 client쪽으로 전달한다.
+    return await this.awsService.uploadFile(file);
+  }
+}
---
+++ src/aws/aws.service.ts
@@ -0,0 +1,63 @@
+// /src/aws/aws.service.ts
+import { HttpException, Injectable } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
+import * as fs from 'fs';
+import * as path from 'path';
+
+// 현재 실행되고 있는 root경로를 확인하고 pa에 저장
+const pa = path.dirname(__dirname).replace('/dist', '');
+
+@Injectable()
+export class AwsService {
+  // S3버킷을 .env파일에서 입력한다.
+  private AWS_S3_BUCKET = process.env.S3_BUCKET;
+  // s3를 사용하기 위해서 IAM계정의 ACCESS, SECRET_ACCESS KEY를 입력한다.
+  private s3 = new AWS.S3({
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  });
+
+  convertBinary(fileName: string) {
+    return fs.readFileSync(pa + '/uploads/' + fileName);
+  }
+
+  // AWS 버킷에 파일 upload
+  async uploadFile(file) {
+    // savename은 실제 S3 버킷에 저장될시 파일 이름을 말합니다
+    const { savename } = file;
+    console.log('savename : ', savename);
+    return await this.s3_upload(
+      file.buffer, // 파일 버퍼(binary file)
+      this.AWS_S3_BUCKET, // 저장할 S3버킷 이름
+      savename, // S3버킷에 저장할 파일 이름
+      file.mimetype, // 파일타입 : 위 코드에서는 'application/octet-stream'을 사용합니다.
+    );
+  }
+
+  // 실제 AWS에 파일을 업로드 하는 method
+  async s3_upload(file, bucket, name, mimetype) {
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+      ACL: 'public-read',
+      ContentType: mimetype,
+      ContentDisposition: 'inline',
+      CreateBucketConfiguration: {
+        LocationConstraint: process.env.s3_REGION,
+      },
+    };
+
+    console.log(params);
+
+    try {
+      let s3Response = await this.s3.upload(params).promise();
+
+      console.log(s3Response);
+      return s3Response;
+    } catch (err) {
+      console.log(err);
+      throw new HttpException(err.message, err.status ? err.status : 500);
+    }
+  }
+}
